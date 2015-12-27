96330d6fda3b9766e4e3b1e32eac2b26f93fffcf
Sylvain Abélard <sylvain.abelard@gmail.com>
2015-12-27 12:26:17 +0100

    with the names to find them
    OK I'll need to have a look at a JS `zip`

--- bookmarklet.js
+++ bookmarklet.js
@@ -6,11 +6,18 @@ var l3s = $('.strength-3').length;
 var l4s = $('.strength-4').length;
 var l5s = $('.strength-5').length;
 
+var m0 = l0s > 3 ? '' : $('.strength-0').parent().parent().children('.skill-badge-name').text();
+var m1 = l1s > 3 ? '' : $('.strength-1').parent().parent().children('.skill-badge-name').text();
+var m2 = l2s > 3 ? '' : $('.strength-2').parent().parent().children('.skill-badge-name').text();
+var m3 = l3s > 3 ? '' : $('.strength-3').parent().parent().children('.skill-badge-name').text();
+var m4 = l4s > 3 ? '' : $('.strength-4').parent().parent().children('.skill-badge-name').text();
+var m5 = l5s > 3 ? '' : $('.strength-5').parent().parent().children('.skill-badge-name').text();
+
 alert("Progress:\n" +
-  (l0s ? (l0s + " x") : '') +
-  (l1s ? (l1s + " *\n") : '') +
-  (l2s ? (l2s + " **\n") : '') +
-  (l3s ? (l3s + " ***\n") : '') +
-  (l4s ? (l4s + " ****\n") : '') +
-  (l5s ? (l5s + " *****\n") : '')
+  (l0s ? (l0s + " x " + m0 + "\n") : '') +
+  (l1s ? (l1s + " * " + m1 + "\n") : '') +
+  (l2s ? (l2s + " ** " + m2 + "\n") : '') +
+  (l3s ? (l3s + " *** " + m3 + "\n") : '') +
+  (l4s ? (l4s + " **** " + m4 + "\n") : '') +
+  (l5s ? (l5s + " ***** " + m5 + "\n") : '')
 )
